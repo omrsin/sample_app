6ed176a6ac11b484faac3bf5b81671245a1f0530
omrsin <omrsin@gmail.com>
2012-05-18 18:26:01 -0430

    Modify spec code

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,10 +4,18 @@ describe "User pages" do
 	
 	subject { page }
 	
+	shared_examples_for "all user pages" do
+	  it { should have_selector('h1', text: heading) }
+  	it { should have_selector('title', text: full_title(page_title)) }
+	end
+	
+	subject { page }
+	
   describe "signup page" do
   	before { visit signup_path }
+  	let(:heading) { 'Sign up' }
+		let(:page_title) { 'Sign up' }
   	
-  	it { should have_selector('h1', text: 'Sign up') }
-  	it { should have_selector('title', text: full_title('Sign up')) }
+  	it_should_behave_like "all user pages" 
   end
 end
