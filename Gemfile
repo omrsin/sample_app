f20596e76d9e4ba1b9cfc3220641d06958be804e
Omar Erminy <omrsin@gmail.com>
2012-05-24 00:37:01 -0430

    Finish user edit, update, index and destroy actions

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.3'
 gem 'bootstrap-sass', '2.0.0'
 gem 'bcrypt-ruby', '3.0.1'
+gem 'faker', '1.0.1'
+gem 'will_paginate', '3.0.3'
+gem 'bootstrap-will_paginate','0.0.6'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -23,7 +26,7 @@ group :assets do
   gem 'coffee-rails', '3.2.2'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  # gem 'therubyracer', :platform => :ruby
+  # gem 'therubyracer', :platform	 => :ruby
 
   gem 'uglifier', '1.2.3'
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,8 @@
 #  created_at      :datetime        not null
 #  updated_at      :datetime        not null
 #  password_digest :string(255)
+#  remember_token  :string(255)
+#  admin           :boolean         default(FALSE)
 #
 
 require 'spec_helper'
@@ -25,9 +27,17 @@ describe User do
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:remember_token) }
+	it { should respond_to(:admin) }
 	it { should respond_to(:authenticate) }
 	
 	it { should be_valid }
+	it { should_not be_admin }
+	
+	describe "with admin attribute" do
+		before { @user.toggle!(:admin) }
+		
+		it { should be_admin }
+	end
 	
 	describe "when name is not present" do
 		before { @user.name = " " }
