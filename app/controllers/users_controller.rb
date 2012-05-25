81631d5e882ea85bf9d214238cbe6ad382b4205c
Omar Erminy <omrsin@gmail.com>
2012-05-24 21:13:20 -0430

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   
   def show
   	@user = User.find(params[:id])
+  	@microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -47,14 +48,7 @@ class UsersController < ApplicationController
   	end
   end
   
-  private
-	
-		def signed_in_user
-			unless signed_in?
-				store_location
-				redirect_to signin_path, notice: "Please sign in." 
-			end
-		end
+  private		
 		
 		def signed_out_user
 			redirect_to root_path, notice: "Please sign out first." unless !signed_in?
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -62,16 +62,26 @@ describe "User pages" do
   	let(:heading) { 'Sign up' }
 		let(:page_title) { 'Sign up' }
   	
-  	it_should_behave_like "all user pages" 
+  	it_should_behave_like "all user pages"  	
   end
   
   describe "profile page" do
   	let(:user) { FactoryGirl.create(:user) }
+  	let!(:m1) { FactoryGirl.create(:micropost, user: user, content: "Foo") }
+  	let!(:m2) { FactoryGirl.create(:micropost, user: user, content: "Bar") }
+  	
   	before { visit user_path(user) }
+  	
   	let(:heading) { user.name }
 		let(:page_title) { user.name }
 		
 		it_should_behave_like "all user pages"
+		
+		describe "microposts" do
+  		it { should have_content(m1.content) }
+  		it { should have_content(m2.content) }
+  		it { should have_content(user.microposts.count) }
+  	end 
   end
   
   describe "signup"do
