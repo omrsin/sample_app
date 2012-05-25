dc29844d6bad5e08b043b89e251b42818c90ec0c
Omar Erminy <omrsin@gmail.com>
2012-05-25 16:12:15 -0430

    Add user following

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -30,7 +30,18 @@ describe "Static pages" do
 				user.feed.each do |item|
 					page.should have_selector("li##{item.id}", text: item.content)
 				end
-			end			
+			end
+			
+			describe "follower/followed counts" do
+				let(:other_user) { FactoryGirl.create(:user) }
+				before do
+					other_user.follow!(user)
+					visit root_path
+				end
+				
+				it { should have_link("0 following", href: following_user_path(user)) }
+				it { should have_link("1 followers", href: followers_user_path(user)) }
+			end
 		end
 	end
 
